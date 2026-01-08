9c5436537bca17e8e9cb56dbbe966713cae47c5f
smarak-mohanty <smarakmohanty290903@gmail.com>
2026-01-08 20:27:29 +0530

    SELECTION Clause

---
+++ selectionClause.sql
@@ -0,0 +1,66 @@
+
+
+/*
+Given an EMP table with the following data:
+EMPNO	ENAME	    JOB	        MGR	    HIREDATE	SAL	    COMM    DEPTNO
+7369	SMITH	    CLERK	    7902	17-DEC-80	800	    NULL    20
+7499	ALLEN	    SALESMAN	7698	20-FEB-81	1600	300     30
+7521	WARD	    SALESMAN	7698	22-FEB-81	1250	500     30
+7566	JONES	    MANAGER	    7839	02-APR-81	2975	NULL    20
+7600	MARTIN	    SALESMAN	7698	28-SEP-81	1250	1400    30
+7698	BLAKE	    MANAGER	    7839	01-MAY-81   2850	NULL    30
+7782	CLARK	    MANAGER	    7839	09-JUN-81   2450	NULL    10
+7839	KING	    PRESIDENT	NULL	17-NOV-81	5000	NULL    10
+7844	SCOTT	    ANALYST	    7566	19-APR-87   3000	NULL    20
+7876	ADAMS	    CLERK	    7788	23-MAY-87	1100	NULL    20
+7900	JAMES	    CLERK	    7698	03-DEC-81   950	    NULL    30  
+*/
+
+
+--! Write a query to display all the details of the employee working in dep_no 20.
+SELECT * 
+FROM EMP
+WHERE DEPTNO=20;
+
+
+--! Write a query to display the employees name whose salaries are greater than 2000 and is from dept_no 20.
+SELECT ENAME
+FROM EMP
+WHERE DEPTNO=20 AND SAL>2000;
+
+
+--! Write a query to display the annual salary of the employee having name James.
+SELECT SAL*12 AS ANNUAL_SAL
+FROM EMP
+WHERE ENAME='JAMES';
+
+
+--! Write a query to display the names of the employee working as clerk
+SELECT ENAME 
+FROM EMP
+WHERE JOB='CLERK';
+
+
+--! Write a query to display the details of the employees whose salary is more than 2000.
+SELECT *
+FROM EMP
+WHERE SAL>2000;
+
+
+--! Write a query to display the name of employes heired after 1981.
+SELECT ENAME 
+FROM EMP
+WHERE HIREDATE>'31-DEC-1981';
+
+
+--! Write a query to display the name of employes whose annual salary is greater than 12000.
+SELECT ENAME
+FROM EMP
+WHERE SAL*12>12000;
+
+
+--! Write a query to display the name of the employee hire after 2 month of 1981.
+SELECT ENAME
+FROM EMP
+WHERE HIREDATE<'01-FEB-1981';
+
